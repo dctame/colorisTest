19407a81917f836dd085dee6a0faf4801ed6bbc6
dctame <tcygankov.a.v@gmail.com>
2018-07-03 03:12:46 +0300

    test task. Done

--- coloristest/models.py
+++ coloristest/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-from django.utils import timezone
-
 
 class Post(models.Model):
     text = models.TextField()
+    words = str(text)
+    words = words.split()
+    output = []
+    for x in words:
+        if x not in output:
+            output.append(x)
+    lenght = len(output)
+            
     def publish(self):
         self.save()
         
--- coloristest/views.py
+++ coloristest/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import PostForm
+from .models import Post
 
-# Create your views here.
+def notes(request):
+    posts = Post.objects.order_by('text')
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        post = form.save()
+        post.save()
+    else:
+        form = PostForm()
+    return render(request, 'coloristest/notes.html', {'form': form, 'posts': posts})
+    
